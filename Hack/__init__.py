0db74a9a72afbad42df4aeed36c5f0cd977fc8d0
hori <horihoribass@gmail.com>
2021-11-09 08:57:29 +0900

    DB接続完了

--- Hack/__init__.py
+++ Hack/__init__.py
@@ -10,66 +10,61 @@ from azure.cosmos import exceptions, CosmosClient, PartitionKey
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    try:
+        logging.info('Python HTTP trigger function processed a request.')
+        endpoint = config["ENDPOINT"]
+        key = config["PRIMARYKEY"]
 
-    # Initialize the Cosmos client
-    endpoint = config["ENDPOINT"]
-    key = config["PRIMARYKEY"]
+        client = CosmosClient(endpoint, key)
 
-    # <create_cosmos_client>
-    client = CosmosClient(endpoint, key)
-    # </create_cosmos_client>
+        database_name = config["DATABASE"]
+        database = client.create_database_if_not_exists(id=database_name)
 
-    # Create a database
-    # <create_database_if_not_exists>
-    database_name = config["DATABASE"]
-    database = client.create_database_if_not_exists(id=database_name)
-    # </create_database_if_not_exists>
+        container_name = config["CONTAINER"]
+        container = database.create_container_if_not_exists(
+            id=container_name,
+            partition_key=PartitionKey(path="/pk"),
+            offer_throughput=400
+        )
 
-    # Create a container
-    # Using a good partition key improves the performance of database operations.
-    # <create_container_if_not_exists>
-    container_name = config["CONTAINER"]
-    container = database.create_container_if_not_exists(
-        id=container_name,
-        partition_key=PartitionKey(path="/pk"),
-        offer_throughput=400
-    )
-    #  </create_container_if_not_exists>
+        maxId = "1"
+        for id in container.query_items(
+                "SELECT VALUE MAX(c.id) FROM c", enable_cross_partition_query=True):
+            maxId = str(id)
 
-    # Query these items using the SQL query syntax.
-    # Specifying the partition key value in the query allows Cosmos DB to retrieve data only from the relevant partitions, which improves performance
-    # <query_items>
-    query = "SELECT * FROM cWHERE c.id = '1'"
+        item = container.read_item(item=maxId, partition_key=maxId)
 
-    item = container.read_item(item="1", partition_key="1")
+        request_charge = container.client_connection.last_response_headers['x-ms-request-charge']
 
-    request_charge = container.client_connection.last_response_headers['x-ms-request-charge']
+        print('Read item with airvolume {0}. Operation consumed {1} request units'.format(
+            item['airvolume'], (request_charge)))
 
-    print('Read item with airvolume {0}. Operation consumed {1} request units'.format(
-        item['airvolume'], (request_charge)))
-    # </query_items>
+        datetime1 = datetime.now().isoformat()
+        airVolume = item['airvolume']
 
-    # TODO CosmosDBへ接続
-    datetime1 = datetime(2021, 11, 11, 12, 00).isoformat()
-    airVolume = "middle"
+        res = {
+            "dateTime": datetime1,
+            "airVolume": airVolume  # weak,normal,strong
+        }
 
-    num = random.randint(1, 10)
-    if num > 7:
-        airVolume = "strong"
-    elif num > 3:
-        airVolume = "normal"
-    else:
-        airVolume = "weak"
+        return func.HttpResponse(
+            body=json.dumps(res),
+            mimetype="application/json",
+            charset="utf-8",
+            status_code=200
+        )
 
-    res = {
-        "dateTime": datetime1,
-        "airVolume": airVolume
-    }
+    except Exception as e:
+        logging.info("エラー発生")
+        logging.info(e)
+        res = {
+            "dateTime": datetime(2021, 11, 11, 12, 00).isoformat(),
+            "airVolume": "weak"
+        }
 
-    return func.HttpResponse(
-        body=json.dumps(res),
-        mimetype="application/json",
-        charset="utf-8",
-        status_code=200
-    )
+        return func.HttpResponse(
+            body=json.dumps(res),
+            mimetype="application/json",
+            charset="utf-8",
+            status_code=200
+        )
