6d55dab9869aad548544ac4767bc842dfc4cec87
hori <horihoribass@gmail.com>
2021-11-09 21:43:19 +0900

    DBに11件以上入らない問題の解決

--- Hack/__init__.py
+++ Hack/__init__.py
@@ -37,16 +37,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             else:
                 airvolumeReq = req_body.get('airvolume')
 
-        # レコードの最大値を取得
-        maxId = "1"
+        # レコードの最大IDを取得
+        totalNum = "1"
         for id in container.query_items(
-                "SELECT VALUE MAX(c.id) FROM c", enable_cross_partition_query=True):
-            maxId = str(id)
+                "SELECT value COUNT(1) FROM c", enable_cross_partition_query=True):
+            totalNum = str(id)
 
         if airvolumeReq:
             # クエリがある場合、新規で追加
-            newId = int(maxId) + 1
-
+            newId = int(totalNum) + 1
             container.create_item(
                 body={"id": str(newId), "pk": str(newId), "airvolume": airvolumeReq})
             return func.HttpResponse(
@@ -57,7 +56,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             )
         else:
             # クエリがない場合、最新のレコードを取得
-            item = container.read_item(item=maxId, partition_key=maxId)
+            item = container.read_item(item=totalNum, partition_key=totalNum)
 
             request_charge = container.client_connection.last_response_headers[
                 'x-ms-request-charge']
